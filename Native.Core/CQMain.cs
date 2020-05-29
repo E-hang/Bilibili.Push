16c1f41bb3ca985a105fb90f6f1b0e4430743b12
E-hang <biuxiaohei@hotmail.com>
2020-05-29 16:19:47 +0800

    2020/05/29 B站推送 版本2.0.0 开始重建

--- Native.Core/CQMain.cs
+++ Native.Core/CQMain.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using me.cqp.hang.bilibilipush.Code.Event;
+using Native.Sdk.Cqp.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,18 +9,19 @@ using Unity;
 
 namespace Native.Core
 {
-	/// <summary>
-	/// 酷Q应用主入口类
-	/// </summary>
-	public class CQMain
-	{
-		/// <summary>
-		/// 在应用被加载时将调用此方法进行事件注册, 请在此方法里向 <see cref="IUnityContainer"/> 容器中注册需要使用的事件
-		/// </summary>
-		/// <param name="container">用于注册的 IOC 容器 </param>
-		public static void Register (IUnityContainer unityContainer)
-		{
-			
-		}
-	}
+    /// <summary>
+    /// 酷Q应用主入口类
+    /// </summary>
+    public class CQMain
+    {
+        /// <summary>
+        /// 在应用被加载时将调用此方法进行事件注册, 请在此方法里向 <see cref="IUnityContainer"/> 容器中注册需要使用的事件
+        /// </summary>
+        /// <param name="container">用于注册的 IOC 容器 </param>
+        public static void Register(IUnityContainer unityContainer)
+        {
+            // 将实现了接口的类注入到容器中, 并且注入的名称就为 Json 中使用的 "name" 字段
+            unityContainer.RegisterType<IGroupMessage, Event_GroupMessage>("群消息处理");
+        }
+    }
 }
